5a4404b5d32229424c9bdbcd4ba65fbe7d7c33fd
steven garcia <sgarcia00@gmail.com>
2021-01-27 22:56:50 +0100

    move test folder for api

--- crawler_consumer/django/consumer/api/test/test_serializers.py
+++ crawler_consumer/django/consumer/api/test/test_serializers.py
@@ -3,27 +3,27 @@ from api.models import Author, ArticleFile, Article, Keyword
 from api.serializers import ArticleSerializer
 from api.api_lib.serializers import ArticleSerializer as apiserializer
 from api.api_lib.validators import ArticleValidator
-from api_tests.article_test_data_1 import *
+from api.test.article_test_data_1 import test_data_1, test_data_2
 import json
+
 
 class ArticleSerializerTC(TestCase):
     def setUp(self) -> None:
         pass
 
-    def serializer_data_1_ok(self):
+    def test_serializer_data_1_ok(self):
         validator = ArticleValidator(test_data_1)
         data = validator.get_validated_data()
-        print("validated data: "+ str(json.dumps(data, indent=4)))
+        print("validated data: " + str(json.dumps(data, indent=4)))
         serializer = ArticleSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         a = serializer.save()
         print(Article.objects.get(digital_object_id=a).to_json())
 
-
-    def serializer_data_2_ok(self):
+    def test_serializer_data_2_ok(self):
         validator = ArticleValidator(test_data_2)
         data = validator.get_validated_data()
-        print("validated data: "+ str(json.dumps(data, indent=4)))
+        print("validated data: " + str(json.dumps(data, indent=4)))
         serializer = ArticleSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         a = serializer.save()
@@ -32,8 +32,8 @@ class ArticleSerializerTC(TestCase):
     def api_serializer_data_2_ok(self):
         validator = ArticleValidator(test_data_2)
         data = validator.get_validated_data()
-        print("validated data: "+ str(json.dumps(data, indent=4)))
+        print("validated data: " + str(json.dumps(data, indent=4)))
         serializer = apiserializer(data=data)
-        #serializer.is_valid(raise_exception=True)
+        # serializer.is_valid(raise_exception=True)
         a = serializer.save()
         print(Article.objects.get(digital_object_id=a).to_json())
