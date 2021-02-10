2f53d2113b5ffa012c7f9185c7a877e57c78bae9
steven garcia <sgarcia00@gmail.com>
2021-02-10 14:53:08 +0100

    finish initial java consumer

--- consumers/java/mdcrawler-consumer/src/main/java/org/perpetualnetworks/mdcrawlerconsumer/config/CrawlerConsumerConfiguration.java
+++ consumers/java/mdcrawler-consumer/src/main/java/org/perpetualnetworks/mdcrawlerconsumer/config/CrawlerConsumerConfiguration.java
@@ -13,7 +13,7 @@ import java.util.Properties;
 
 @Data
 @Component
-@ConfigurationProperties("crawlerConsumer")
+@ConfigurationProperties("crawler-consumer")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
